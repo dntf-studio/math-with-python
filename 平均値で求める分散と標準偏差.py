b8ab229c9a702b6c188af4079166bc4b8597ae87
dntf <*****@users.noreply.github.com>
2021-02-13 13:31:40 +0900

    平均値

---
+++ 平均値で求める分散と標準偏差.py
@@ -0,0 +1,20 @@
+import statistics
+import math
+#-----------------平均値で求める分散と標準偏差--------------------
+q_nrs = [7,5,7,6,8,7,10,6]
+q_nrs_len = len(q_nrs)
+q_nrs_sum = sum(q_nrs)
+q_nrs_mean = q_nrs_sum/q_nrs_len
+print('mean:',q_nrs_mean)
+
+q_nrs_al = 0
+for q_nrs_each in q_nrs:
+    q_nrs_each = q_nrs_each**2
+    q_nrs_al +=  q_nrs_each
+
+q_nrs_1x2_mean = q_nrs_al/q_nrs_len
+print(q_nrs_1x2_mean)
+q_nrs_ss = q_nrs_1x2_mean-q_nrs_mean**2
+print('分散=',q_nrs_ss)
+q_nrs_aa = math.sqrt(q_nrs_ss)
+print('標準偏差=',round(q_nrs_aa,1))
