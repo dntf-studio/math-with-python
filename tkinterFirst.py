6cb5d806da16ad9421bcded98dd724b3062f8b56
dntf <*****@users.noreply.github.com>
2021-02-26 20:04:27 +0900

    tkinter

--- tkinterFirst.py
+++ tkinterFirst.py
@@ -10,7 +10,9 @@ canvas = tk.Canvas(root,height=HEIGHT,width=WIDTH)
 canvas.pack()
 
 def helloCallBack():
-    tkmsg.showinfo("Hello!!Python!!","Hello World")
+    #txt = entry.get()
+    text.set("You put:"+str(entry.get()))
+    #tkmsg.showinfo("Hello!!Python!!","Hello World")
     print('loaded')
 
 frame = tk.Frame(root,bg="#e6e6e6")
@@ -21,6 +23,12 @@ button.place(x=210,y=1)
 
 label = tk.Label(frame,text="This is a label",bg='white')
 label.place(x=1,y=1)
+
+text = tk.StringVar(frame)
+text.set("aaaa")
+
+label2 = tk.Label(frame,textvariable=text)
+label2.place(x=1,y=30)
 
 entry = tk.Entry(frame)
 entry.place(x=80,y=2)
